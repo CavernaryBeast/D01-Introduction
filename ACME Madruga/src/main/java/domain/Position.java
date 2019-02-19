5807623e47a13c3b67fb47059edd2b6222e6b0ca
CavernaryBeast <raimundokarate98@gmail.com>
2019-02-19 21:37:44 +0100

    Conceptual, UML and Java Domain Model finished
    Position has been done

--- ACME Madruga/src/main/java/domain/Position.java
+++ ACME Madruga/src/main/java/domain/Position.java
@@ -1,6 +1,31 @@
+
 package domain;
 
+import org.hibernate.validator.constraints.NotBlank;
 
-public class Position {
+public class Position extends DomainEntity {
+
+	//Atributos de clase
+	private String	positionEs;
+	private String	positionEn;
+
+
+	@NotBlank
+	public String getPositionEs() {
+		return this.positionEs;
+	}
+
+	public void setPositionEs(final String positionEs) {
+		this.positionEs = positionEs;
+	}
+
+	@NotBlank
+	public String getPositionEn() {
+		return this.positionEn;
+	}
+
+	public void setPositionEn(final String positionEn) {
+		this.positionEn = positionEn;
+	}
 
 }
